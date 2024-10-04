c80b116e004d12b078e0e4e3cc4791efc611d963
Davinson-Okorum <davinson.canaveral@okorum.com>
2024-10-04 09:32:19 -0500

    Update Styles with Tailwind

--- meteor-wallet/imports/ui/ContactForm.jsx
+++ meteor-wallet/imports/ui/ContactForm.jsx
@@ -29,37 +29,61 @@ function ContactForm() {
   };
 
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          value={DataContact.name}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={DataContact.email}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="imageURL">Image URL</label>
-        <input
-          type="text"
-          id="imageURL"
-          value={DataContact.imageURL}
-          onChange={handleChange}
-        />
-        <div>
-          <button type="button" onClick={SaveContact}>
-            Save Contact
-          </button>
+    <form className="mt-6">
+      <div className="grid grid-cols-6 gap-6">
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700  "
+          >
+            Name
+          </label>
+          <input
+            id="name"
+            type="text"
+            value={DataContact.name}
+            onChange={handleChange}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700  "
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={DataContact.email}
+            onChange={handleChange}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label
+            htmlFor="imageURL"
+            className="block text-sm font-medium text-gray-700  "
+          >
+            Image URL
+          </label>
+          <input
+            type="text"
+            id="imageURL"
+            value={DataContact.imageURL}
+            onChange={handleChange}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+          <div className="px-2 py-3 text-right">
+            <button
+              type="button"
+              onClick={SaveContact}
+              className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+            >
+              Save Contact
+            </button>
+          </div>
         </div>
       </div>
     </form>
